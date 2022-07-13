ab4f1230024f1e77af1a0a67e074380efc5a79ac
Leonardo Pinheiro <60147713+leonardpin-br@users.noreply.github.com>
2022-07-13 14:13:02 -0300

    working argument existance

--- scripts/unittest-skeleton-generator.sh
+++ scripts/unittest-skeleton-generator.sh
@@ -19,8 +19,6 @@ include() {
 # Included files
 include "utils.sh"
 
-
-
 # Removes Cygwin prefix.
 # param1 (string): The root directory path.
 removesCygwinPrefix() {
@@ -77,25 +75,33 @@ unittestSkeletonGenerator() {
     # Receives the ${fileBasename} (filename with extension).
     local file_to_be_tested="$1"
 
-    # Verifies if the argument was passed.
+    # Verifies if the argument was passed, and if the file exists.
+    # https://stackoverflow.com/a/21164441
     if [ -f $file_to_be_tested ]; then
         echo -e "This script receives, as argument, the name of the python file (with extension) that is going to be tested."
+        exit 1
+    elif [[ -z `find /this/is/path -name $file_to_be_tested` ]]; then
+        echo -e "File not found."
         exit 1
     fi
 
     # Builds the full file path to the file that is going to be tested.
     local file_relative_folder=$(get_file_relative_folder "$file_to_be_tested")
-    local file_full_folder_path=$(realpath $file_relative_folder)
-    local file_full_path="${file_full_folder_path}/${file_to_be_tested}"
-
-    local ROOT_DIR=$(get_root_directory)
-
-    # Creates the full folder path for the test file that will be created.
-    local tests_full_folder_path="${ROOT_DIR}/tests"
-    # local tests_full_folder_path=${file_full_folder_path/src/"$tests_replacement"}
+    # local file_full_folder_path=$(realpath $file_relative_folder)
+    # local file_full_path="${file_full_folder_path}/${file_to_be_tested}"
 
 
-    echo -e "$tests_full_folder_path"
+    echo -e "$file_relative_folder"
+
+
+    # local ROOT_DIR=$(get_root_directory)
+
+    # # Creates the full folder path for the test file that will be created.
+    # local tests_full_folder_path="${ROOT_DIR}/tests"
+    # # local tests_full_folder_path=${file_full_folder_path/src/"$tests_replacement"}
+
+
+    # echo -e "$tests_full_folder_path"
 
     # # Discovers the environment this script is running on:
     # # see   https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux
